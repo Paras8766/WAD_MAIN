82d9ef83eda39d2ff65836ea13a7200f1f59ea3c
Paras Thakur <tparas576@gmail.com>
2026-05-04 12:24:36 +0530

    update mongoose version and fix song routes import

--- w18/server.js
+++ w18/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const songRoutes = require("./models/songModel");
+const songRoutes = require("./routes/songRoutes");
+
 
 const app = express();
 
@@ -8,8 +9,8 @@ connectDB();
 
 app.use(express.json());
 
-app.use("/", songRoutes);  
+app.use("/", songRoutes);
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
